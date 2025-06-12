d63e8050ecf0160be54904fb190efa91731c4dde
sonymuhamad <sony.muhamad@satudental.com>
2025-06-12 19:25:03 +0700

    init

---
+++ src/main/java/com/sony/coffeshopauth/CoffeShopAuthApplication.java
@@ -0,0 +1,13 @@
+package com.sony.coffeshopauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeShopAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeShopAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sony/coffeshopauth/CoffeShopAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sony.coffeshopauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeShopAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
